7c2a8c91a2a60ac0f1fad5dae8cce8d9cba421d3
jarneamerlinck <jarneamerlinck@gmail.com>
2023-06-25 10:42:49 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+# Setup nixos home manager
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# add home manager requirements
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
+nix-channel --update
+
+
+#cd into home
+cd /home;
+cd "$(find . -maxdepth 1 -type d |grep /)";
+mkdir -p .config/nixpkgs;
+
+curl https://raw.githubusercontent.com/jarneamerlinck/nixos-configs/main/home.nix > .config/nixpkgs/home.nix
+
+
+# rebuild and check configs
+nixos-rebuild switch
